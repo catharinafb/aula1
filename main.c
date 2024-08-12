1601b2b1d0326f52168fd327994775b08e3d438e
CATHARINA FRAZATTO BOLOGNINI <catharina.fb@puccampinas.edu.br>
2024-08-12 11:44:39 -0300

    meu primeiro commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float a;1000;
+    float b;500;
+    float c;
+
+
+
+    c = a + b;
+
+    printf(" resultado: %d", c);
+    return 0;
+
+}
+
